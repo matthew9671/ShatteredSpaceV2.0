57920ecad460b93d979b5750737dadb69f22f3c9
Jingxing Yang <jingxiny@usc.edu>
2017-04-13 18:38:56 -0700

    uploaded tracking mine

--- framework/ryany.cs
+++ framework/ryany.cs
@@ -50,12 +50,137 @@ public class ryanTest
 // ###################################################################
 // Write your weapon class here
 // ###################################################################
+public class trackingMineAttack_t : attack_t
+{
+    private unit_t target;
+    public trackingMineAttack_t(unit_t target) : base(null)
+    {
+        this.target = target;
+    }
+    public unit_t getTarget()
+    {
+        return target;
+    }
+}
 
-// public class trackingMine_t : weapon_t
-// {
+public class trackingMine_t : weapon_t
+{
+    static int[] modules = { 0, 1, 1, 0 };
+    int splashDamage = 2;
+    public trackingMine_t():base(range:1,damage:4,delay:-1,modules:modules)
+    {       
+    }
+    public void fire(attack_t attack,board_t board,unit_t master)
+    {
+        trackingMineAttack_t tma = (trackingMineAttack_t)attack;
+        mine m = new mine(tma.getTarget(),board);
+        board.put_object(master.get_pos(),m);
+    }
 
-// }
+}
 
+public class mine:unit_t
+{
+    private board_t board;
+    private int turn = 0;
+    private unit_t target;
+    private int centerDmg = 4;
+    private int splashDmg = 2;
+    public mine (unit_t target, board_t board):base("mine",1)
+    {
+        this.target = target;
+        this.board = board;
+        stepLife = -1;
+        turnLife = 2;
+        solid = false;
+        exists = false;
+    }
+    public void end_turn()
+    {
+        if(stepLife<0)
+        {
+            stepLife = 3;
+        }
+    }
+
+    public void step_update()
+    {
+        stepLife--;
+        if(stepLife<0)
+        {
+            return;
+        }
+        if (stepLife == 0)
+        {
+            on_destroy();
+            return;
+        }
+        Vector2 dif = target.get_pos() - this.get_pos();
+        int dirX = Math.Abs(dif.x) / dif.x; // 1 if the target is on the right, else -1
+        int dirY = Math.Abs(dif.x) / dif.y;// 1 if the the target is on the top, else -1
+        Vector2 newX = this.get_pos() + new Vector2(dirX, 0); // new pos after changing x
+        Vector2 newY = this.get_pos() + new Vector2(0, dirY); // new pos after changing y
+        int disX = SS.distance(newX,target.get_pos()); // distance when moving mine through x axis;
+        int disY = SS.distance(newY, target.get_pos()); // distance when moving mine through y axis;
+        board.remove_object(this);
+        //maybe don't need to check if the new location is on board
+        if (disX<disY)
+        {
+            this.set_pos(newX);
+            if (!board.is_free(newX))
+            {
+                on_destroy();//explode in new position? 
+                return;
+            }
+            board.put_object(newX, this);
+        }
+        else if(disX>disY)
+        {
+            this.set_pos(newY);
+            if (!board.is_free(newY))
+            {
+                on_destroy();//explode in new position? 
+                return;
+            }
+            board.put_object(newY, this);
+        }
+        else
+        {
+            Random rand = new Random();
+            int option = rand.Next(2);
+            Vector2 newPos = this.get_pos() + new Vector2(dirX * option, dirY * (1 - option));
+            this.set_pos(newPos);
+            if (!board.is_free(newPos))
+            {
+                on_destroy();//explode in new position? 
+                return;
+            }
+            board.put_object(newPos, this);
+        }
+
+        
+    }
+
+    public void on_destroy()
+    {
+        Vector2 pos = this.get_pos();
+        //center damage,
+        damage_t cd = new damage_t();
+        cd.set_params(centerDmg, 0);
+        cd.set_pos(pos);
+        cd.stepLife = 1;
+        board.create_damage(cd);
+        //splash damage
+        for(int i=0; i<6; i++)
+        {
+            damage_t sd = new damage_t();
+            sd.set_params(splashDmg, 0);
+            sd.set_pos(pos + SS.DIRECTIONS[i]);
+            sd.stepLife = 1;
+            board.create_damage(sd);
+        }
+    }
+}
 // ###################################################################
 // To build a class in C# you need to have a constructor
 // Here is an example of the simplest weapon class to get you started
